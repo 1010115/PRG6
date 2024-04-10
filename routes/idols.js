905b0edce3a16dc091311cc5000814c401e754a3
Zino Schubert <zrs.schubert@gmail.com>
2024-04-10 13:17:51 +0200

    refactoring

--- routes/idols.js
+++ routes/idols.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Idol = require('../models/Idol');
-const {getIdols, createIdol, updateIdol, deleteIdol, getIdol, optionsIdols, optionsIdol} = require("../controllers/idols");
+const {
+    getIdols,
+    createIdol,
+    updateIdol,
+    deleteIdol,
+    getIdol,
+    optionsIdols,
+    optionsIdol
+} = require("../controllers/idols");
 
-router.route('/').get(getIdols).post(createIdol).options(optionsIdols)
-router.route('/:id').put(updateIdol).delete(deleteIdol).get(getIdol).options(optionsIdol)
+router.route('/').get(getIdols)
+    .post(createIdol)
+    .options(optionsIdols)
+router.route('/:id').put(updateIdol).delete(deleteIdol)
+    .get(getIdol)
+    .options(optionsIdol)
 
 
-
-module.exports =  router;
+module.exports = router;
 
